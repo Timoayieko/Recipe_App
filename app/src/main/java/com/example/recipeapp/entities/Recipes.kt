153ac1de0bae9884787509d6b87075e3dc7ba297
iBosire <allan.bosire@strathmore.edu>
2024-01-04 13:19:48 +0300

    finished splash screen UI

---
+++ app/src/main/java/com/example/recipeapp/entities/Recipes.kt
@@ -0,0 +1,9 @@
+package com.example.recipeapp.entities
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity( tableName = "Recipes")
+data class Recipes(
+    @PrimaryKey(autoGenerate = true)
+    var id:Int
+)
